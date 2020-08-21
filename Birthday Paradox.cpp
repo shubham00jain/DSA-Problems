625e905571c4ba876a1396d17913495c3ae3f434
Shubham Jain <shubham656jain@gmail.com>
2020-08-22 01:55:18 +0530

    Create Birthday Paradox.cpp

---
+++ Birthday Paradox.cpp
@@ -0,0 +1,11 @@
+/*
+Statement: How many people must be there in a room to make the probability 100% that at-least two people in the room have same birthday?
+
+This problem is more related to mathematics than to programming.
+The answer to the above is simple its 367 people. (counting leap years as well).
+
+General case: What is the probability that two persons among n have same birthday, given bday percentage?
+
+see explaination on gfg
+https://www.geeksforgeeks.org/birthday-paradox/
+/*
