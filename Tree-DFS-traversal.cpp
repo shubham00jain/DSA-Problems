570f5a5a13b8b164216c624922c3fe40fb211433
Shubham Jain <shubham656jain@gmail.com>
2020-08-27 14:34:19 +0530

    Create Tree-DFS-traversal.cpp

---
+++ Tree-DFS-traversal.cpp
@@ -0,0 +1,20 @@
+/*
+A tree is a non-linear data structure. It is often termed as a "hierarchical or recursive data structure".
+In graph thoery, A tree is a directed acyclic graph (DAG).
+It is made up of a user-defined class/ structure.
+Most common tree is binary tree in which each node can have max 2 children.
+The data members of a typical BT are:
+1. data value
+2. left child pointer 
+3. right child pointer
+Strict Binary tree - each node has exactly 0 or 2 children.
+Complete Binary tree- When making the level wise array(queue) of BT, there is no empty space in between.
+Full Binary tree- A tree in which each node has 2 children, except the leaf nodes.
+
+There are mainly 2 types of tree traversal.
+1. DFS - going along the depth of the tree while following some common rule. Its general sub-types are: Pre-order(Root,L,R), In-order(L,Root,Right), Post-order(Left,Right,Root).
+2. BFS - going along the breath of the tree while following some common rule.
+*/
+
+//------------------recursive code for DFS BT traversals-------------------------
+
