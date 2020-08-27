c049dfe4ec12b89691f59c9a8062fdb216bfa706
Shubham Jain <shubham656jain@gmail.com>
2020-08-27 17:07:05 +0530

    Create Tree-Traversal.cpp

---
+++ Tree-Traversal.cpp
@@ -0,0 +1,35 @@
+/*
+A tree is a special type of graph, so it typically has 2 types of traversal:
+1. DFS - access the nodes while going through depth of the tree.
+2. BFS - access the nodes while going thorough breadth of the tree. 
+
+
+
+*/
+
+
+void inorder( node * root){
+ if(node!=NULL){
+   inorder(root->left);
+   cout<<root->data;
+   inorder(root->right);
+ }
+}
+
+
+void preorder( node * root){
+ if(node!=NULL){
+   cout<<root->data;
+   preorder(root->left);
+   preorder(root->right);
+ }
+}
+
+
+void postorder( node * root){
+ if(node!=NULL){
+   preorder(root->left);
+   preorder(root->right);
+   cout<<root->data;
+ }
+}
