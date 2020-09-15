be551f5b833d8c6ca2a78fcc7f5f50680b9967f5
Shubham Jain <shubham656jain@gmail.com>
2020-09-15 21:02:11 +0530

    Update sum-of-nodes.cpp

--- trees/sum-of-nodes.cpp
+++ trees/sum-of-nodes.cpp
@@ -7,3 +7,38 @@ long int sumBT(Node* root)
     
     return sumBT(root->left) + sumBT(root->right) + root->key;
 }
+
+//--------------------------------------------------------------------
+//------------------------USING QUEUE (WITHOUT RECURSION)-------------
+//--------------------------------------------------------------------
+
+long int sumBT(Node* root)
+{
+    if(!root)
+        return 0;
+    
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    long int sum = 0;
+    
+    while(!q.empty()){
+        Node * curr = q.front();
+        q.pop();
+        if(curr){
+            sum += curr->key;
+            if(curr->left)
+                q.push(curr->left);
+                
+            if(curr->right)
+                q.push(curr->right);
+        }
+            
+        if(!curr){
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+    }
+    return sum;
+}
