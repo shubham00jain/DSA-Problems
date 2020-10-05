e45ba7112e1a73e7eabeefbecd3d1d9945975e47
Shubham Jain <shubham656jain@gmail.com>
2020-10-06 03:13:26 +0530

    Update bfs-ques.cpp

--- Graphs/bfs-ques.cpp
+++ Graphs/bfs-ques.cpp
@@ -5,27 +5,22 @@
 using namespace std;
 
 int solve(vector<int> g[], int N, unordered_map<int,int> umap){
-
     queue<int> q;
     q.push(1);
     q.push(-1);
     vector<int> vis(N+1,0);
     int ans = INT_MAX;
     int find = 0;
-
     while(!q.empty()){
         int curr = q.front();
         q.pop();
-
         if(curr == -1){
             if(find==1)
                 break;
-
             if(!q.empty())
                 q.push(-1);
             continue;
         }
-
         if(!vis[curr]){
             vis[curr] = 1;
             if(umap.find(curr)!=umap.end()){
@@ -35,31 +30,25 @@ int solve(vector<int> g[], int N, unordered_map<int,int> umap){
                 }
             }
         }
-
         for(auto i : g[curr]){
             if(!vis[i]){
                 q.push(i);              
             }
         }
-    }
-        
+    }  
     return ans;
-
 }
 
 int main(){
-
     int N;
     cin>>N;
     vector<int> adj[N+1];
-
     for(int i=0 ; i<N-1 ; i++){
         int u,v;
         cin>>u>>v;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    
     // for(int i=0;i<N;i++){
     //     cout<<i <<": ";
     //     for(auto x : adj[i])
@@ -67,13 +56,9 @@ int main(){
     
     //     cout<<endl;
     // }
-    
-    
-
     unordered_map<int, int> umap;
     int k;
     cin>>k;
-
     for(int i =0; i<k;i++){
         int t;
         cin>>t;
