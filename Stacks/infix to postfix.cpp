5cbb77902fc9b20b60999b143990cab424becdc9
Shubham Jain <shubham656jain@gmail.com>
2020-11-22 21:43:24 +0530

    Update infix to postfix.cpp

--- Stacks/infix to postfix.cpp
+++ Stacks/infix to postfix.cpp
@@ -1,4 +1,17 @@
 //https://practice.geeksforgeeks.org/problems/infix-to-postfix-1587115620/1
+/*
+To evaluate a postfix expression:
+1. Start reading the string from left to right.
+2. Push if to stack if any number or operand is encountered.
+3. If any operator is encountered, pop 2 elements out of the stack, perform the operation and push the resultant into the stack again.
+
+To evaluate a prefix expression, do the same thing as postfix but read the string from right to left instead.
+
+To convert infix to prefix, its almost the same as this code having these diffenrences:
+1. Read the infix string from right to left.
+2. As we reading the infix string from right to left, every ) bracket becomes our "opening" bracket and every ( bracket becomes our "closing" bracket.
+3. Use the above the exceptions and do the same as infix to postfix to a get a string as a resullt. Now reverse the answer to the final result.
+*/
 
 
 bool checkop(char c){
