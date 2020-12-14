fad199d9f394b099cbaf4023de45670cbdf47746
Shubham Jain <shubham656jain@gmail.com>
2020-12-15 04:27:17 +0530

    Create permutation of strings.cpp

---
+++ Strings/permutation of strings.cpp
@@ -0,0 +1,36 @@
+//https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
+
+void solve(string s, int l, int r, map<string,int>& mp){
+    if(l==r){
+        mp[s] = 1;   
+    }
+        
+    for(int i = l; i<=r; i++){
+        swap( s[i],s[l]);
+        solve(s,l+1,r,mp );
+        swap(s[i],s[l]);
+    }
+    
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--){
+	    string s;
+	    cin>>s;
+	    map<string,int> mp;
+	    solve(s,0,s.size()-1,mp);
+	    for(auto i : mp){
+	        cout<<i.first<<" ";
+	    }
+	    
+	    cout<<endl;
+	}
+	
+	return 0;
+}
