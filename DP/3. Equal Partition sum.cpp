65371774d27957c3bab1edd8666b3bb5f5b9514f
Shubham Jain <shubham656jain@gmail.com>
2021-02-04 22:23:44 +0530

    Update 3. Equal Partition sum.cpp

--- DP/3. Equal Partition sum.cpp
+++ DP/3. Equal Partition sum.cpp
@@ -40,4 +40,45 @@ public:
 };
 
 //Bottom up iterative-------------------------------------------------------------
+//--------------------------------------------------------------------------------
+
+class Solution{
+public:
+
+
+    int equalPartition(int N, int arr[])
+    {
+        int sum = accumulate(arr,arr+N,0);
+        if(sum%2!=0)
+            return 0;
+        sum = sum/2;
+        
+        vector<vector<int>> dp(N+2, vector<int>(sum+2, -1) );
+        
+        for(int i=0;i<N+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(i==0)
+                    dp[i][j] = 0;
+                if(j==0)
+                    dp[i][j] = 1;
+            }
+        }
+        
+        for(int i=1;i<N+1;i++){
+            for(int j=1;j<sum+1;j++){
+                
+                if( arr[i-1] > j )
+                    dp[i][j] = dp[i-1][j];
+                else{
+                    if( dp[i-1][j-arr[i-1]] || dp[i-1][j])
+                        dp[i][j] = 1;
+                    else dp[i][j] = 0;
+                }
+            }
+        }
+        
+        return dp[N][sum];
+        
+    }
+};
 
